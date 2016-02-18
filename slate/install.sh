ebd02cce827d40bc63be229fb944b45042cec8ca
helmedeiros <helio.cabralmedeiros@gmail.com>
2016-02-18 10:23:31 -0200

    closes #3. After install we add Slate to start login system configuration

--- slate/install.sh
+++ slate/install.sh
@@ -9,3 +9,6 @@ if test ! $(pgrep -f "Slate.app")
 then
  open /Applications/Slate.app
 fi
+
+
+osascript -e 'tell application "System Events" to make login item at end with properties {path:"/Applications/Slate.app", hidden:false}'  > /dev/null 2>&1
