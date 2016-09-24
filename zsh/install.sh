c1aefeec73e36f8410e10cc45f260e9ee7311560
helmedeiros <667210+helmedeiros@users.noreply.github.com>
2016-09-24 16:19:29 -0300

    zsh premissions needed for sierra

--- zsh/install.sh
+++ zsh/install.sh
@@ -3,5 +3,10 @@
 if test ! $(which zsh)
 then
  curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
+
+ cd /usr/local/share/
+ sudo chmod -R 755 zsh
+ sudo chown -R root:staff zsh
+
  chsh -s /bin/zsh
 fi
