f16b42b78468026f1f29226177ec3319e0d982cd
helmedeiros <667210+helmedeiros@users.noreply.github.com>
2016-10-29 21:21:26 -0700

    adding docker start

--- docker/install.sh
+++ docker/install.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 #
-docker --version
-docker-compose --version
-docker-machine --version
+#!/bin/sh
+if test ! $(pgrep -f "Docker.app" | head -1)
+then
+ open /Applications/Docker.app
+ docker --version
+ docker-compose --version
+ docker-machine --version
+fi
