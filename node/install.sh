09ee72581bb40f8fc468d47782612eb55029df34
Helio Medeiros <Helio_Medeiros@gap.com>
2017-10-04 11:35:22 -0300

    Closes #21 Should be easy to install npm packages Why? Today I don't have any defined way to install npm packages How? Create an easy way to add new npm packages to list

--- node/install.sh
+++ node/install.sh
@@ -1,7 +1,25 @@
 #!/usr/bin/env bash
-
+#
+# npm
+#
+# This installs npm packages using npm.
 function installglobal() {
 	npm install -g "${@}" 2> /dev/null
 }
 
-installglobal spoof
+# Check for npm
+if test $(which npm)
+then
+  echo "  Installing npm packages for you."
+
+	installglobal spoof
+	installglobal express
+	installglobal request
+	installglobal mocha
+	installglobal harp
+	installglobal grunt-cli
+	installglobal grunt
+	installglobal gulp
+	installglobal speed-test
+	installglobal newman
+fi
