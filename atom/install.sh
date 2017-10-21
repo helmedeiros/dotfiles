34137fb89b51663f9ca009920f54e57864f3c4b4
helmedeiros <667210+helmedeiros@users.noreply.github.com>
2017-10-21 00:28:35 -0200

    Closes #24 Apm login was made when starts not present
    Why is this need?
    You'll need this token to get automatic updates in Atom and to publish packages to the Atom package registry.
    
    How is this solves?
    Given we can't find stars, requests the APM's login.

--- atom/install.sh
+++ atom/install.sh
@@ -1,2 +1,18 @@
-apm star --installed  
-apm stars --install
+#!/bin/sh
+#
+# Atom
+set -e
+
+declare -r -i installed=$(apm list -i| awk -F '[()]' '{print $2}')
+
+# Update ATOM Packages
+if [[ $(which apm) && $installed > 0 ]]
+then
+  echo "  Configuring Atom Packages."
+  apm stars
+  apm star --installed
+  apm stars --install
+else
+  echo "  Create Atom Packages Token."
+  apm login
+fi
