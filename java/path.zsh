663abc5d5d013871a28762be665997d2c2f06a1a
helmedeiros <667210+helmedeiros@users.noreply.github.com>
2018-02-15 16:49:51 -0200

    close #32 JDK8 will be install too

--- java/path.zsh
+++ java/path.zsh
@@ -1,3 +1,3 @@
-launchctl setenv JAVA_HOME $(/usr/libexec/java_home)
-export JAVA_HOME=$(/usr/libexec/java_home)
+launchctl setenv JAVA_HOME $(/usr/libexec/java_home -v '1.8*')
+export JAVA_HOME=$(/usr/libexec/java_home -v '1.8*')
 export PATH="$JAVA_HOME/bin:$PATH"
