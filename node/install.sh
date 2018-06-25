198851b0e1aa2aacb7fda04676081223b238afe8
helmedeiros <helio.cabralmedeiros@gmail.com>
2018-06-24 21:32:40 -0300

    install and manage npm via nvm

--- node/install.sh
+++ node/install.sh
@@ -7,6 +7,32 @@ function installglobal() {
 	npm install -g "${@}" 2> /dev/null
 }
 
+function getLatest() {
+	cd "$NVM_DIR"
+	git fetch --tags origin
+	git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
+}
+
+function installNVM() {
+	local -r dot_nvm="$HOME/.nvm"
+
+	if ! [[ -d "${dot_nvm}" || -L "${dot_nvm}" ]]
+	then
+		git clone https://github.com/creationix/nvm.git ~/.nvm
+		getLatest
+		source nvm.sh
+		nvm install 8.11.
+	else
+		getLatest
+	fi
+}
+
+if test $(which node)
+then
+	echo " Installing npm manually"
+	intallNVM
+fi
+
 # Check for npm
 if test $(which npm)
 then
@@ -22,4 +48,5 @@ then
 	installglobal gulp
 	installglobal speed-test
 	installglobal newman
+	installglobal @angular/cli@latest
 fi
