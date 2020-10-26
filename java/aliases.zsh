3d00d44783cb82652c686b0abd3993cca27717c9
helmedeiros <667210+helmedeiros@users.noreply.github.com>
2020-10-26 15:26:08 +0100

    new brew and java

--- java/aliases.zsh
+++ java/aliases.zsh
@@ -1,7 +1,7 @@
 export JAVA_HOME_8=$(/usr/libexec/java_home -v '1.8*')
 export JAVA_HOME_11=$(/usr/libexec/java_home -v '11*')
-export JAVA_HOME_13=$(/usr/libexec/java_home -v '13*')
+export JAVA_HOME_14=$(/usr/libexec/java_home -v '14*')
 
 alias java8='export JAVA_HOME=$JAVA_HOME_8; launchctl setenv JAVA_HOME $JAVA_HOME_8; export PATH="$JAVA_HOME/bin:$PATH"; java -version'
 alias java11='export JAVA_HOME=$JAVA_HOME_11; launchctl setenv JAVA_HOME $JAVA_HOME_11; export PATH="$JAVA_HOME/bin:$PATH"; java -version'
-alias java13='export JAVA_HOME=$JAVA_HOME_13; launchctl setenv JAVA_HOME $JAVA_HOME_13; export PATH="$JAVA_HOME/bin:$PATH"; java -version'
+alias java14='export JAVA_HOME=$JAVA_HOME_14; launchctl setenv JAVA_HOME $JAVA_HOME_14; export PATH="$JAVA_HOME/bin:$PATH"; java -version'
