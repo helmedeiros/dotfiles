8748dbf03b7a0e7c7e748ad57fdd58bef91d1b18
Helio Medeiros <helio.cabralmedeiros@gmail.com>
2022-10-05 17:38:38 +0200

    upgrade to M1

--- go/install.sh
+++ go/install.sh
@@ -4,8 +4,8 @@
 #
 # This installs go.
 function installglobal() {
-  echo " > go get ${@}"
-  go get -u "${@}"
+  echo " > go install ${@}@latest"
+  go install "${@}@latest"
 }
 
 if test $(which go)
@@ -16,13 +16,8 @@ then
 
   installglobal golang.org/x/tools/cmd/goimports
   installglobal golang.org/x/tools/cmd/gorename
-  installglobal github.com/sqs/goreturns
   installglobal github.com/nsf/gocode
-  installglobal github.com/alecthomas/gometalinter
   installglobal github.com/zmb3/gogetdoc
-  installglobal github.com/zmb3/goaddimport
   installglobal github.com/rogpeppe/godef
-  installglobal github.com/gorilla/mux
-  installglobal github.com/lib/pq
-  installglobal github.com/spf13/cobra/cobra
+  installglobal github.com/spf13/cobra-cli
 fi
